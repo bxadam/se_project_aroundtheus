5196537a4a2376c51e6a4f650326be982d80f53a
bxadam <bx.adam03@gmail.com>
2024-04-01 17:33:51 -0400

    added modal function and updated close button type

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,15 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const profileEditBtn = document.querySelector("#profile-edit-btn");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileModalClose = document.querySelector("#modal-close-btn");
+
+profileEditBtn.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
+
+profileModalClose.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
